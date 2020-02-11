51a6c8cb97d6d49ebdd57a1657200719648a4a61
Faye Van Roekel <flvanroekel@my365.bellevue.edu>
2020-02-11 17:59:44 -0600

    assignment 7.3

--- Week-7/vanroekel-form-validation/src/app/app.component.ts
+++ Week-7/vanroekel-form-validation/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tarvin-form-validation';
+  title = 'vanroekel-form-validation';
 
   onSubmit(formData) {
     console.log(formData);
